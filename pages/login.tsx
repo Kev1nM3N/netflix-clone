52d95d09fcf6d74cf258d3ce4664416ad24c2b34
Kevin Mendez <mediaman502@aol.com>
2023-11-21 16:34:49 -0500

    added packages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-//17:10
+//27:42
+
+interface Inputs {
+  email: string
+  password: string
+}
 
 function login() {
+  const [login, setLogIn] = useState(false)
+  const { register, handleSubmit, watch, formState: { errors }, } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+
+
+
   return (
     <div
       className="relative flex h-screen w-screen flex-col bg-black md:items-center
@@ -29,16 +42,25 @@ function login() {
             height={150}
         />
 
-        <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
+        <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0
             md:max-w-md md:px-14">
-            <h1>Sign In</h1>
+            <h1 className="text-4xl font-semibold">Sign In</h1>
             <div className="space-y-4">
-                <label>
-                    <input type="email" placeholder="Email" className="input"/>
+                <label className="inline-block w-full">
+                    <input type="email" placeholder="Email" className="input" {...register("email", {required: true})}/>
                 </label>
-                <label>
-                    <input type="password" placeholder="Password" className="input"/>
+                <label className="inline-block w-full">
+                    <input type="password" placeholder="Password" className="input" {...register("password", {required: true})}/>
                 </label>
+            </div>
+
+            <button className="w-full rounded bg-[#e50914] py-3 font-semibold">Sign In</button>
+
+            <div className="text-[gray]">
+              New to Netflix?{` `}
+              <button type="submit" className="text-white hover:underline">Sign Up Now</button>
             </div>
         </form>
     </div>
