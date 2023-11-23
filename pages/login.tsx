4a01d1d0d76c79b960d4f253500d99e280cbb8b0
Kevin Mendez <mediaman502@aol.com>
2023-11-23 14:57:45 -0500

    added login functions

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-//27:42
-
 interface Inputs {
   email: string
   password: string
@@ -13,10 +11,14 @@ interface Inputs {
 
 function login() {
   const [login, setLogIn] = useState(false)
-  const { register, handleSubmit, watch, formState: { errors }, } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-
-
+  const { register, handleSubmit, formState: { errors }, } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+      if (login){
+        // await signIn(email, password)
+      } else{
+        // await signUp(email, password)
+      }
+  }
 
   return (
     <div
@@ -50,17 +52,21 @@ function login() {
             <div className="space-y-4">
                 <label className="inline-block w-full">
                     <input type="email" placeholder="Email" className="input" {...register("email", {required: true})}/>
+                    {errors.email && (<p className="p-1 text-[13px] font-light  text-orange-500">Please enter a valid email</p>)}
                 </label>
                 <label className="inline-block w-full">
                     <input type="password" placeholder="Password" className="input" {...register("password", {required: true})}/>
+                    {errors.password && (<p className="p-1 text-[13px] font-light  text-orange-500">Please enter a valid password</p>)}
                 </label>
             </div>
 
-            <button className="w-full rounded bg-[#e50914] py-3 font-semibold">Sign In</button>
+            <button className="w-full rounded bg-[#e50914] py-3 font-semibold" 
+            onClick={() => setLogIn(true)}>Sign In</button>
 
             <div className="text-[gray]">
               New to Netflix?{` `}
-              <button type="submit" className="text-white hover:underline">Sign Up Now</button>
+              <button type="submit" className="text-white hover:underline"
+              onClick={() => setLogIn(false)}>Sign Up Now</button>
             </div>
         </form>
     </div>
